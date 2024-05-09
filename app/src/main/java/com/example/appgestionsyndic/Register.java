0618f43863e5e17cbd42075968621f8424fabd32
ahmedmakroum <ahmedmakroum3@gmail.com>
2024-05-09 22:05:00 +0100

    second commit added layout for login

--- app/src/main/java/com/example/appgestionsyndic/Register.java
+++ app/src/main/java/com/example/appgestionsyndic/Register.java
@@ -1,6 +1,8 @@
 package com.example.appgestionsyndic;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,19 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // In your activity or fragment where you initialize the Spinner
+
+        Spinner spinnerRole = findViewById(R.id.spinnerRole);
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.role_array, android.R.layout.simple_spinner_item);
+
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// Apply the adapter to the spinner
+        spinnerRole.setAdapter(adapter);
     }
 }
